bf4d5e11c7ba4942700b6f2d918b691fd3199b86
peppyeben <austineben34@gmail.com>
2024-12-02 14:43:43 +0100

    Modified tab width.

--- contentScript.js
+++ contentScript.js
@@ -1,34 +1,34 @@
-;(() => {
-  let mailer, mailing
-  console.log('I am activeeeeeeee')
+(() => {
+    let mailer, mailing;
+    console.log("I am activeeeeeeee");
 
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, value, videoId } = obj
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
 
-    newMailLoaded()
-    if (type === 'NEW') {
-    }
-  })
+        newMailLoaded();
+        if (type === "NEW") {
+        }
+    });
 
-  const newMailLoaded = () => {
-    const summarizer = document.getElementsByClassName('bHJ')[0]
-    console.log('sumarizer: ', summarizer)
+    const newMailLoaded = () => {
+        const summarizer = document.getElementsByClassName("bHJ")[0];
+        console.log("sumarizer: ", summarizer);
 
-    if (summarizer) {
-      const sumbutton = document.createElement('button')
-      sumbutton.textContent = 'summarize!!1'
-      sumbutton.addEventListener('click', sumarize)
-      summarizer.prepend(sumbutton)
-    }
-  }
+        if (summarizer) {
+            const sumbutton = document.createElement("button");
+            sumbutton.textContent = "summarize!!1";
+            sumbutton.addEventListener("click", sumarize);
+            summarizer.prepend(sumbutton);
+        }
+    };
 
-  const sumarize = () => {
-    const content = document.getElementById(':1u')
-    const body = content.getElementsByTagName('div')
-    const realContent = body[0]
+    const sumarize = () => {
+        const content = document.getElementById(":1u");
+        const body = content.getElementsByTagName("div");
+        const realContent = body[0];
 
-    console.log(content)
-    console.log(body)
-    console.log(realContent.innerText)
-  }
-})()
+        console.log(content);
+        console.log(body);
+        console.log(realContent.innerText);
+    };
+})();
